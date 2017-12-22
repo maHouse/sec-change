4d3a33cc79afaabf11a08f1faebbcf87168a1220
maHouse <1037243021@qq.com>
2017-12-22 23:10:43 +0800

    add-plugins

--- jQuery学习/plugin.js
+++ jQuery学习/plugin.js
@@ -1,5 +1,5 @@
 ;( function( $ ) {
-	jQuery.fn.extend( {
+	// jQuery.fn.extend( {
 
 	// 	"color" : function( value ) {
 	// 		return this.css( "color", value );
@@ -10,11 +10,14 @@
 	// 	}
 	// } );
 
+	// 面向对象解决上述两个方法合并的问题
+	
+	
 	var Beautifier = function( ele, opt ) {
 
 		this.$element = ele;
 		this.defaults = { 'color' : 'red'};
-		this.options = $.extend( {}, defaults, opt );
+		this.options = $.extend( {}, this.defaults, opt );
 	}
 
 	Beautifier.prototype = {
--- jQuery学习/realcode.js
+++ jQuery学习/realcode.js
@@ -3,5 +3,7 @@ $( function() {
 	// alert( $("div").color() + " \n返回字符串，证明插件可用");
 
 	// alert( $("div").font() + " \n返回object证明得到的是jQuery对象");
+	
 
+	alert( $("div").myPlugin() + "\n返回字符串，证明插件可用ff");
 } )
