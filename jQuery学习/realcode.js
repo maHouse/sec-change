205d931c456306a73fc0c7bcf43ebda448b59cb5
maHouse <1037243021@qq.com>
2017-12-22 22:56:32 +0800

    add-plugins

--- jQuery学习/realcode.js
+++ jQuery学习/realcode.js
@@ -1,7 +1,7 @@
 $( function() {
 
-	alert( $("div").color() + " \n返回字符串，证明插件可用");
+	// alert( $("div").color() + " \n返回字符串，证明插件可用");
 
-	alert( $("div").font() + " \n返回object证明得到的是jQuery对象")
+	// alert( $("div").font() + " \n返回object证明得到的是jQuery对象");
 
 } )
